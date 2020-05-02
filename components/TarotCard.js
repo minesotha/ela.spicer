65cd35e7ca6a102c4ecbc697ecbcd4071d704f7f
Maja Trzepacz <minesotha@gmail.com>
2020-05-02 02:02:03 +0200

    filled card descriptions

--- components/TarotCard.js
+++ components/TarotCard.js
@@ -9,19 +9,37 @@ template.innerHTML = `
     position: absolute;
     transition: 0.6s all;
     cursor:pointer;
-    opacity:1;
 }
 .card:hover {
     box-shadow: -1px -1px 8px 5px rgba(100, 100, 255, 0.6), 0 6px 20px 0 rgba(5, 60, 150, 0.5);
-    opacity:0.5;
 }
 .hidden{
     display:none;
 }
+explainer{
+    margin: 0 auto;
+    display: block;
+    position: absolute;
+}
+#explainerText{
+    z-index: 5;
+    position: absolute;
+    padding: 2vh;
+    color: black;
+    font-style: italic;
+    font-size: 2.2vh;
+}
+#front{
+    opacity:1;
+    transition: 0.6s all;
+}
+#front:hover{
+    opacity:0.5;
+}
 </style>
 <img id="front" class="card hidden"/>
 <img id="back" class="card"/>
-
+<div id="explainer" class="hidden"></div>
 `;
 
 
@@ -32,22 +50,47 @@ class TarotCard extends HTMLElement {
         shadowRoot.appendChild(template.content.cloneNode(true));
 
         this.$front = shadowRoot.querySelector('#front');
-
         if (this.hasAttribute('img')) {
             var imgAddress = this.getAttribute('img');
             this.$front.src = imgAddress;
         }
 
         this.$back = shadowRoot.querySelector('#back');
-
         if (this.hasAttribute('backImg')) {
             var imgAddress = this.getAttribute('backImg');
             this.$back.src = imgAddress;
         }
 
+        this.$exp = shadowRoot.querySelector('#explainer');
+
+        if (this.hasAttribute('explainerBackground')) {
+
+            var img = document.createElement('img');
+            var imgAddress = this.getAttribute('explainerBackground');
+            img.id = "explainerBackground";
+            img.classList.add("card");
+            img.src = imgAddress;
+            this.$exp.appendChild(img);
+        }
+
+        // this.$label = shadowRoot.querySelector('#explainerText');
+        if (this.hasAttribute('explainerText')) {
+            var label = document.createElement('label');
+            var text = this.getAttribute('explainerText');
+            label.textContent = text;
+            label.id = "explainerText";
+            this.$exp.appendChild(label);
+        }
+
+
         this.onclick = () => {
-            this.$front.classList.remove("hidden");
-            this.$back.classList.add("hidden");
+            if (this.$front.classList.contains("hidden")) {
+                this.$front.classList.remove("hidden");
+                this.$back.classList.add("hidden");
+            }
+            else {
+                this.$exp.classList.toggle("hidden");
+            }
         }
 
     }
--- tarot/tarot.js
+++ tarot/tarot.js
@@ -3,30 +3,79 @@ async function getCards() {
     $("custom-instruction").addClass("hidden");
 
     var cards = [];
-    while (cards.length <4){
+    while (cards.length < 4) {
         cards.push(getRandomCard(cards));
     }
     for (let i = 0; i < cards.length; i++) {
         const element = cards[i];
-        var card = $('<tarot-card id="card'+i+'" img="../projekt/tarot/'+element+'.png" backImg="../projekt/tarot/card_back.png"></tarot-card>');
-        card.css('opacity','0');
+        var cardText = getCardText(element);
+        var card = $('<tarot-card id="card' + i + '" img="../projekt/tarot/' + element + '.png" backImg="../projekt/tarot/card_back.png" explainerBackground="../projekt/tarot/explainer.jpg" explainerText="'+cardText+'"></tarot-card>');
+        card.css('opacity', '0');
         card.animate({
             opacity: 1
-          }, 1000 );
-        $("#gameboardTable").append(card);  
+        }, 1000);
+        $("#gameboardTable").append(card);
         await new Promise(r => setTimeout(r, 1000));
     }
 }
 
-function getRandomCard(cards){
+function getRandomCard(cards) {
     var newCard = 0;
-    while (cards.indexOf(newCard) > -1 || newCard==0) {
-        newCard = Math.floor(Math.random() * 13)+1;
+    while (cards.indexOf(newCard) > -1 || newCard == 0) {
+        newCard = Math.floor(Math.random() * 13) + 1;
     }
     return newCard;
 }
 
-
-function toggleInstruction(){
+function toggleInstruction() {
     $("custom-instruction").toggleClass("hidden");
+}
+
+
+function getCardText(number) {
+    var text = '';
+    switch (number) {
+        case 1:
+            text = `Wschodzące słońce - reprezentuje osobiste korzyści i sukcesy. Korzystaj z przewagi pojawiających się okazji.`
+            break;
+        case 2:
+            text = `Głodomór - Wiedza, która jest Ci powierzona. Masz wystarczając wiedzę by móc bez obaw zanurzyć się w miłości i użyć wsparcia, które Ci oferują.`
+            break;
+        case 3:
+            text = `Jaszczur (Spetarian)- Nie bój się wyrażać swoje uczucia, nawet złość. Karta ta reprezentuje pasje, nieznane i wszystko co jest tabu.`
+            break;
+        case 4:
+            text = `Król - Reprezentuje dobrość, miłosierdzie i przebaczenie. Leczenie się i dawanie sobie szansy. Musisz sobie zaufać.`
+            break;
+        case 5:
+            text = `Królowa - Ta karta reprezentuje Twoją wewnętrzną siłe, pewność siebie i umiejętności. Królowa to przywódca, który decyduje. `
+            break;
+        case 6:
+            text = `Księżyc w pełni - Akcje powzięte w ramach potencjału. Konflikty mogą być piękne. Rozważ swoje postrzeganie. `
+            break;
+        case 7:
+            text = `Księżniczka - ukochana przyjaciółka, kobieta która ma na Ciebie wpływ. Sojusznik, który zawsze oferuje dobre rady. `
+            break;
+        case 8:
+            text = `Mistrz ksiąg - Reprezentuje wyzwania, poświęcenia, konsekwencje. Stawianie czoła rzeczywistości. `
+            break;
+        case 9:
+            text = `Prawdziwa miłość -  Znajdujesz się na rozdrożu. Pamiętaj kim jesteś. Karta ta reprezentuje równieć wspólnotę i unię z drugą osobą. `
+            break;
+        case 10:
+            text = `Spadająca gwiazda - Reprezentuje Twóje połączenie z naturą. Refleksja i cicha konemplacja pomogą Ci uwolnić się i  być swoją własną zaufaną sobą. `
+            break;
+        case 11:
+            text = `Śmierć - Oznacza zarówno koniec jak i początek. Nie bój się iść naprzód! Pozbądź się przeszłości, która może Ci ciążyć.`
+            break;
+        case 12:
+            text = `Wiszący kot - Decyzje, które nam ciążą. Pozbądź się przeszłosci, która Cię dręczy. Rzeczy, które są poza zasięgiem Twojej kontroli. `
+            break;
+        case 13:
+            text = `Wola walki-  Reprezentuje zwycięstwo. Odnajdź siłę w swoim celu, do którego zostałeś stworzony i osiągnięciach, których dokonałeś. Przyznaj i pogódź się ze wstydem lub porażką.`
+            break;
+        default:
+            break;
+    }
+    return text;
 }
